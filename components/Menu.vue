d44244b6afa6c0713548589fa5a73c20f6ef43a7
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2025-04-15 16:05:30 +0700

    update code

--- components/Menu.vue
+++ components/Menu.vue
@@ -5,23 +5,45 @@
             :key="index"
             :item="item"
             :active-item-id="activeItemId"
-            @activate="setActiveItem" />
+            :open-menu-ids="openMenuIds"
+            @activate="setActiveItem"
+            @toggle-menu="toggleMenu" />
     </ul>
 </template>
 
 <script setup>
 import menuData from "@/assets/menu.json";
-const flatMenu = menuData.menu[0];
-
 import { ref } from "vue";
 
 const activeItemId = ref(null);
+const openMenuIds = ref(new Set()); // Use a Set to track multiple open menus
 
 function setActiveItem(id) {
     activeItemId.value = id;
 }
 
-const menuItems = ref(flatMenu);
+function toggleMenu(id) {
+    // Toggle this specific menu ID
+    if (openMenuIds.value.has(id)) {
+        // If it's already open, close it and all its children
+        const newOpenMenus = new Set();
+        // Keep only the menus that aren't the toggled menu or its children
+        openMenuIds.value.forEach(menuId => {
+            if (menuId !== id) {
+                newOpenMenus.add(menuId);
+            }
+        });
+        openMenuIds.value = newOpenMenus;
+    } else {
+        // If it's closed, open it
+        const newOpenMenus = new Set(openMenuIds.value);
+        newOpenMenus.add(id);
+        openMenuIds.value = newOpenMenus;
+    }
+}
+
+// Your menu data
+const menuItems = ref(menuData.menu);
 </script> -->
 
 <template>
@@ -31,7 +53,7 @@ const menuItems = ref(flatMenu);
             :key="index"
             :item="item"
             :active-item-id="activeItemId"
-            :open-item-id="openItemId"
+            :open-menu-ids="openMenuIds"
             @activate="setActiveItem"
             @toggle-menu="toggleMenu" />
     </ul>
@@ -39,24 +61,47 @@ const menuItems = ref(flatMenu);
 
 <script setup>
 import menuData from "@/assets/menu.json";
-const flatMenu = menuData.menu[0];
-
+const flatMenu = menuData.menu;
 import { ref } from "vue";
 
 const activeItemId = ref(null);
-const openItemId = ref(null);
+const openMenuIds = ref(new Set()); // Use a Set to track multiple open menus
+
+// Keep track of top-level menu IDs
+const topLevelMenuIds = flatMenu.map((item) => item.GroupID);
 
 function setActiveItem(id) {
     activeItemId.value = id;
 }
 
 function toggleMenu(id) {
-    // If clicking the same menu that's already open, close it
-    if (openItemId.value === id) {
-        openItemId.value = null;
+    // Check if this is a top-level menu item
+    const isTopLevel = topLevelMenuIds.includes(id);
+
+    // Toggle this specific menu ID
+    if (openMenuIds.value.has(id)) {
+        // If it's already open, close it and all its children
+        const newOpenMenus = new Set();
+        // Keep only the menus that aren't the toggled menu or its children
+        openMenuIds.value.forEach((menuId) => {
+            if (menuId !== id) {
+                newOpenMenus.add(menuId);
+            }
+        });
+        openMenuIds.value = newOpenMenus;
     } else {
-        // Otherwise, open the clicked menu and close any other
-        openItemId.value = id;
+        // If it's closed, open it
+        const newOpenMenus = new Set(openMenuIds.value);
+
+        // If this is a top-level menu, close other top-level menus
+        if (isTopLevel) {
+            topLevelMenuIds.forEach((topId) => {
+                newOpenMenus.delete(topId);
+            });
+        }
+
+        newOpenMenus.add(id);
+        openMenuIds.value = newOpenMenus;
     }
 }
 
--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -1,84 +1,3 @@
-<!-- <template>
-    <li class="relative">
-        <div @click="handleClick">
-            <span
-                v-if="item.Children"
-                class="flex text-lg justify-between px-4 py-2 hover:bg-[#f7f7f7] cursor-pointer select-none nav-item-link"
-                :class="{
-                    'text-[#ca2628]': isActiveItem || hasActiveChild,
-                    'text-black': !isActiveItem && !hasActiveChild,
-                }">
-                {{ item.GroupName }}
-                <NuxtImg
-                    src="/icons/icon-arrow.svg"
-                    width="12px"
-                    alt="arrow"
-                    class="rotate-270" />
-            </span>
-
-            <a
-                v-else
-                :href="`/category/${item.Slug}`"
-                class="block px-4 py-2 hover:bg-[#f7f7f7] cursor-pointer select-none flex text-lg nav-item-link"
-                :class="{ 'text-[#ca2628]': isActiveItem }">
-                {{ item.GroupName }}
-            </a>
-        </div>
-
-        <ul
-            v-if="item.Children && isOpen"
-            class="absolute top-0 left-full w-48 bg-[#f7f7f7] shadow-lg z-10">
-            <MenuItem
-                v-for="(child, index) in item.Children"
-                :key="index"
-                :item="child"
-                :active-item-id="activeItemId"
-                @activate="onActivate" />
-        </ul>
-    </li>
-</template>
-
-<script setup>
-import { ref, computed } from "vue";
-
-const props = defineProps({
-    item: Object,
-    activeItemId: Number,
-});
-
-const emit = defineEmits(["activate"]);
-
-const isOpen = ref(false);
-const hasActiveChildren = ref(false);
-
-// Check if this item is currently active
-const isActiveItem = computed(() => {
-    return props.activeItemId === props.item.GroupID;
-});
-
-// Check if any children are active
-const hasActiveChild = computed(() => {
-    return hasActiveChildren.value;
-});
-
-// Handle child item activation
-function onActivate(id, isChildActive) {
-    hasActiveChildren.value = isChildActive;
-    emit("activate", id, id === props.item.GroupID || isChildActive);
-}
-
-function handleClick() {
-    if (props.item.Children) {
-        isOpen.value = !isOpen.value;
-        // Activate this item
-        emit("activate", props.item.GroupID, true);
-    } else {
-        // Activate this item
-        emit("activate", props.item.GroupID, true);
-    }
-}
-</script> -->
-
 <template>
     <li class="relative">
         <div @click="handleClick">
@@ -114,7 +33,7 @@ function handleClick() {
                 :key="index"
                 :item="child"
                 :active-item-id="activeItemId"
-                :open-item-id="openItemId"
+                :open-menu-ids="openMenuIds"
                 @activate="onActivate"
                 @toggle-menu="onToggleMenu" />
         </ul>
@@ -122,19 +41,19 @@ function handleClick() {
 </template>
 
 <script setup>
-import { ref, computed, watch } from "vue";
+import { ref, computed } from "vue";
 
 const props = defineProps({
     item: Object,
     activeItemId: Number,
-    openItemId: Number,
+    openMenuIds: Object // Set of open menu IDs
 });
 
 const emit = defineEmits(["activate", "toggle-menu"]);
 
-// Instead of local isOpen, derive from parent openItemId
+// Check if this menu is open by looking in the Set
 const isOpen = computed(() => {
-    return props.openItemId === props.item.GroupID;
+    return props.openMenuIds && props.openMenuIds.has(props.item.GroupID);
 });
 
 const hasActiveChildren = ref(false);
@@ -167,7 +86,7 @@ function handleClick() {
         // Activate this item
         emit("activate", props.item.GroupID, true);
     } else {
-        // Activate this item
+        // Activate this item without toggling (no children)
         emit("activate", props.item.GroupID, true);
     }
 }
