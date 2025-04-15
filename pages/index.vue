a130e1d3e5a8d70b7c5d1a3f3681b8c1707d7477
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2025-04-15 14:08:30 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,38 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-useHead({ title: "Yen Market" });
+useHead({
+    title: "Yen Market",
+    meta: [
+        {
+            name: "description",
+            content:
+                "Yen Market cung cấp các sản phẩm tổ yến, nước yến, và thực phẩm bổ dưỡng giúp nâng cao sức khỏe cho mọi gia đình.",
+        },
+        {
+            name: "keywords",
+            content:
+                "Yen Market, tổ yến, yến sào, nước yến, thực phẩm chức năng",
+        },
+        {
+            property: "og:title",
+            content: "Yen Market - Thực phẩm bổ dưỡng từ tổ yến",
+        },
+        {
+            property: "og:description",
+            content:
+                "Khám phá các sản phẩm yến sào cao cấp tại Yen Market – nâng tầm sức khỏe từ thiên nhiên.",
+        },
+        {
+            property: "og:image",
+            content: "/logo-brand.png", // Thay bằng ảnh thật của bạn
+        },
+        {
+            property: "og:url",
+            content: "https://nuxt3-phi-six.vercel.app/",
+        },
+    ],
+});
 
 const isLoading = ref(true);
 
